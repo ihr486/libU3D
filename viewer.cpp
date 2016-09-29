974e3a6ea124b8997e7d5b3b6d4a0f89a626a046
Hiroka Ihara <ihara_h@live.jp>
2016-09-29 11:14:57 +0900

    Moved shader compilation to shader module

--- viewer.cpp
+++ viewer.cpp
@@ -25,55 +25,14 @@ void Viewer::render()
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 }
 
-static GLuint create_shader(const char *vertex_source, const char *fragment_source)
+static void Viewer::load_projection_matrix(GLuint index, GLfloat fovx, GLfloat aspect, GLfloat near, GLfloat far)
 {
-    GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-    GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-
-    glShaderSource(vertex_shader, 1, &vertex_source, NULL);
-    glCompileShader(vertex_shader);
-
-    GLint result, length;
-    glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &result);
-    if(result == GL_FALSE) {
-        glGetShaderiv(vertex_shader, GL_INFO_LOG_LENGTH, &length);
-        if(length > 0) {
-            char *log_msg = new char[length];
-            glGetShaderInfoLog(vertex_shader, length, NULL, log_msg);
-            delete[] log_msg;
-        }
-    }
-
-    glShaderSource(fragment_shader, 1, &fragment_source, NULL);
-    glCompileShader(fragment_shader);
-
-    glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &result);
-    if(result == GL_FALSE) {
-        glGetShaderiv(fragment_shader, GL_INFO_LOG_LENGTH, &length);
-        if(length > 0) {
-            char *log_msg = new char[length];
-            glGetShaderInfoLog(fragment_shader, length, NULL, log_msg);
-            delete[] log_msg;
-        }
-    }
-
-    GLuint program = glCreateProgram();
-    glAttachShader(program, vertex_shader);
-    glAttachShader(program, fragment_shader);
-    glLinkProgram(program);
-
-    glGetProgramiv(program, GL_LINK_STATUS, &result);
-    if(result == GL_FALSE) {
-        glGetProgramiv(program, GL_INFO_LOG_LENGTH, &length);
-        if(length > 0) {
-            char *log_msg = new char[length];
-            glGetProgramInfoLog(program, length, NULL, log_msg);
-            delete[] log_msg;
-        }
-    }
-
-    glDeleteShader(vertex_shader);
-    glDeleteShader(fragment_shader);
-
-    return program;
+    GLfloat f = 1 / tanf(fovx);
+    GLfloat mat[16] = {
+        f, 0, 0, 0,
+        0, f / aspect, 0, 0,
+        0, 0, (near + far) / (near - far), 2 * far * near / (near - far),
+        0, 0, -1, 0
+    };
+    glUniformMatrix4fv(index, 1, GL_FALSE, mat);
 }
